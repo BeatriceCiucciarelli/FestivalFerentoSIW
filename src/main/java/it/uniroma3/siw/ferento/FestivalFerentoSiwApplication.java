15c70b1c53d20c502fc35215fd70882aa786216a
BeatriceCiucciarelli <bea.ciucciarelli@stud.uniroma3.it>
2026-07-24 18:08:37 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/ferento/FestivalFerentoSiwApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.ferento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalFerentoSiwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalFerentoSiwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/ferento/FestivalFerentoSiwApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.ferento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalFerentoSiwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
